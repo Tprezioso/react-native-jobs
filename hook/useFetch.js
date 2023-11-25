7b1b00c0be7a468239b053e9b943b296e7cf63c4
Thomas <tommyprezioso@gmail.com>
2023-11-25 12:26:28 -0500

    added to git ignore

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import {RAPID_API_KEY} from '@env';
+import {RAPID_API_KEY} from '@env';
 
-// const rapidAPIKey = RAPID_API_KEY;
+const rapidAPIKey = RAPID_API_KEY;
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
@@ -14,7 +14,7 @@ const useFetch = (endpoint, query) => {
         params: { ...query },
         headers: {
             'X-RapidAPI-Key':
-            'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4KzuUzVLzA',
+            rapidAPIKey,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         }
     };
